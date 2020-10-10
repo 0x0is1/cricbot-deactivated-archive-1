772443740f6731954ebc9e1b86234624229bcc09
0x0is1 <42772562+0x0is1@users.noreply.github.com>
2020-10-10 10:13:20 +0530

    test

--- cricbot.py
+++ cricbot.py
@@ -9,21 +9,25 @@ choices = ["CSK", "RR"]
 
 base_url = 'https://www.cricbuzz.com/cricket-match/'
 
+
 class Urls:
     URL1 = base_url + 'live-scores'
     URL2 = base_url + 'live-scores/upcoming-matches'
     #URL3 = 'https://www.cricbuzz.com/api/html/cricket-scorecard/30350'
     URL3 = 'https://www.cricbuzz.com/api/cricket-match/commentary/30354'
+
+
 def soup(url):
     response = requests.get(url)
     bsoup = scrapper(response.content, 'html.parser')
     return bsoup
 
+
 iterator = 0
 
 
-
 client = commands.Bot(command_prefix='*')
+
 
 @client.event
 async def on_ready():
@@ -37,12 +41,14 @@ async def score(ctx, index_number=0):
     except:
         await ctx.send('No match available at position {}'.format(str(index_number)))
 
+
 @client.command()
 async def winner(ctx):
     try:
         await ctx.send(random.choice(choices))
     except:
         await ctx.send('Oops.. something went wroong.')
+
 
 @client.command()
 async def schedule(ctx, index_number=0):
@@ -51,6 +57,7 @@ async def schedule(ctx, index_number=0):
     except:
         await ctx.send('No match available for {}'.format(str(index_number)))
 
+
 @client.command()
 async def status(ctx):
     try:
@@ -58,10 +65,12 @@ async def status(ctx):
     except:
         await ctx.send('Match on the way.')
 
+
 @client.command()
 async def disconnect(ctx):
     await ctx.send('Ok. Bye.')
     await client.logout()
 
-auth_token = os.environ.get('DISCORD_BOT_TOKEN')
-client.run(auth_token)
+# auth_token = os.environ.get('DISCORD_BOT_TOKEN')
+auth_token = open('api_key.txt', 'r')
+client.run(auth_token.read())
